30e40bffdf052ce7544d84d285a2db622269c8e9
Rameshkumar1803 <139784575+Rameshkumar1803@users.noreply.github.com>
2025-08-03 01:40:38 +0530

    .

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="bg-gray-900 text-white py-4 text-center mt-10">
-      <p className="text-sm">Made with ❤️ using React & Tailwind CSS</p>
+      <p className="text-sm">Made with ❤️ </p>
       <a
         href="https://github.com/Rameshkumar1803/MealDB-app"
         target="_blank"
